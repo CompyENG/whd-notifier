c3b1985e432c8da2ca2167ff023a992f2d64df37
Bobby Graese <bobby.graese@gmail.com>
2012-07-01 23:31:39 -0500

    Make it possible to clear saved passwords
    Carryover from official addon -- now fixed.  I'm not exactly sure what
    the check for a non-blank password before storing it is, but this
    removes that check so the user can easily blank their password.

--- background.js
+++ background.js
@@ -62,14 +62,22 @@ function getHostedServer() {
 function setUserPass(username, password) {
     if(localStorage.saveUserPass === "true" ) {
         localStorage.username = username;
-        if( password != "*****" && password != "" ) {
-            localStorage.md5password = b64_md5( password );
+        if( password != "*****" ) {
+            if( password == "" ) {
+                localStorage.md5password = "";
+            } else {
+                localStorage.md5password = b64_md5( password );
+            }
             //passwordTextbox.value = "*****";
         }
     } else {
         sessionStorage.username = username;
-        if( password != "*****" && password != "" ) {
-            sessionStorage.md5password = b64_md5( password );
+        if( password != "*****" ) {
+            if( password == "" ) {
+                sessionStorage.md5password = "";
+            } else {
+                sessionStorage.md5password = b64_md5( password );
+            }
             //passwordTextbox.value = "*****";
         }
     }
